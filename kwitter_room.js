407df492f7a529afe589124bda1251aedcc0de3f
naina-coder <82169500+naina-coder@users.noreply.github.com>
2021-09-10 22:11:33 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyCakObD7sMg_dqzWcaYV8PvibmdrLMJ6CE",
+   authDomain: "aesthetic-kwitter.firebaseapp.com",
+   projectId: "aesthetic-kwitter",
+   storageBucket: "aesthetic-kwitter.appspot.com",
+   messagingSenderId: "511165134580",
+   appId: "1:511165134580:web:cce459eab4859f68f690a1"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
